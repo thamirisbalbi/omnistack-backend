7d019cb5b5fa89bd16001ff83525101e16a10f85
Thamiris Balbi <thamiris.balbi@estudante.ufjf.br>
2022-01-19 14:43:46 -0300

    Adiciona informações sobre rota e recursos

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express'); //importando funcionalidades do módulo expr
 // agora a variável express contém todas as funcionalidades disponíveis para o projeto
 const app = express();
 
+/**
+Rota: conjunto completo do link localhost / Recursos: o que vem depois da /. caminho para a rota.
+**/
+
 app.get('/', (request, response) => {
     return response.json({
         evento: 'Semana Omnistack 11.0',
