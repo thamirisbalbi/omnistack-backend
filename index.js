1d58e6bc9a01666654fac89931f8e2862b327c0d
Thamiris Balbi <thamiris.balbi@estudante.ufjf.br>
2022-01-19 11:58:09 -0300

    Importa funcionalidades para o projeto

---
+++ index.js
@@ -0,0 +1,2 @@
+const express = require('express'); //importando funcionalidades do módulo express para dentro da variável express
+// agora a variável express contém todas as funcionalidades disponíveis para o projeto
