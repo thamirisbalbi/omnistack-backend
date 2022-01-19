d7aee3a3f30d2e6c9ab0d3bc15048441d2eab331
Thamiris Balbi <thamiris.balbi@estudante.ufjf.br>
2022-01-19 12:03:07 -0300

    Manda aplicação ouvir a porta 3333

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const express = require('express'); //importando funcionalidades do módulo express para dentro da variável express
 // agora a variável express contém todas as funcionalidades disponíveis para o projeto
 const app = express();
+
+app.listen(3333);
