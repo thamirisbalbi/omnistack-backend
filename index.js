a3971a1e131401eb36ad3fe8bf90b1be28564e55
Thamiris Balbi <thamiris.balbi@estudante.ufjf.br>
2022-01-19 12:02:02 -0300

    Cria variável para armazenar aplicação

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 const express = require('express'); //importando funcionalidades do módulo express para dentro da variável express
 // agora a variável express contém todas as funcionalidades disponíveis para o projeto
+const app = express();
