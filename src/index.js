b4a6f498fe1987d7256928fa3323c781211ba94f
Thamiris Balbi <thamiris.balbi@estudante.ufjf.br>
2022-01-26 07:34:36 -0300

    Cria uma variável para as rotas do express

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express'); //importando funcionalidades do módulo express para dentro da variável express
+// agora a variável express contém todas as funcionalidades disponíveis para o projeto
+const app = express();
+
+app.use(express.json()); //transforma o arquivo json do parâmetro em algo entendível pela aplicação
+
+/**
+Rota: conjunto completo do link localhost / Recursos: o que vem depois da /. caminho para a rota.
+**/
+
+/**
+ Métodos HTTP
+ GET: buscar (retornar) uma informação do backend: utilizado quando acesso a rota no computador
+ POST: Criar uma informação no back-end
+ PUT: Alterar uma informação no back-end
+ DELETE: Deletar uma informação no back-end
+ o navegador utiliza para navegar a página somente o método GET
+**/
+
+app.post('/users', (request, response) => {
+    const body = request.body;
+
+    console.log(body);
+
+    return response.json({
+        evento: 'Semana Omnistack 11.0',
+        aluno: 'Cleiton Souza'
+    });
+});
+app.listen(3333); //cannot get: ainda não criou uma rota para aplicaçao
