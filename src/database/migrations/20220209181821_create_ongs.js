06c2b4c3da292facd5babd65b0aa0ee0c80f6822
Thamiris Balbi <thamirisbalbi2277@gmail.com>
2022-02-16 02:36:32 -0300

    Cria componentes da tabela

--- src/database/migrations/20220209181821_create_ongs.js
+++ src/database/migrations/20220209181821_create_ongs.js
@@ -1,6 +1,13 @@
 
 exports.up = function(knex) {
- 
+    knex.schema.createTable('ongs', function(table){
+        table.string('id').primary();
+        table.string('name').notNullable(); //nome da tabela, não pode ser nulo
+        table.string('email').notNullable();
+        table.string('whatsapp').notNullable();
+        table.string('city').notNullable();
+        table.string('uf', 2).notNullable(); //estado, passando o tamanho do texto armazenado dentro
+    }); //criar uma nova tabela com nome ongs, recebendo uma função de segundo parâmetro, que recebe a tabela como parâmetro
 };
 
 exports.down = function(knex) {
