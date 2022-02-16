05c66b643c14a4967e6290a284387f0d2ff87816
Thamiris Balbi <thamirisbalbi2277@gmail.com>
2022-02-16 04:30:43 -0300

    importa arquivo connection para routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const express = require('express'); //importa a aplicação express para o arquivo routes
 const crypto = require('crypto');
+const connection = require('./database/connection');
 
 const routes = express.Router(); //desacopla as rotas do express em uma nova variável
 
