785c648f07c02b4c5e46a532be25dc88405360c6
Thamiris Balbi <thamirisbalbi2277@gmail.com>
2022-02-16 04:27:38 -0300

    Exporta a conexão com o banco de dados

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,5 +1,6 @@
 const knex = require('knex');
 const configuration = require('../../knexfile');
 
-const conection = knex(configuration.development);
+const conection = knex(configuration.development); //passa como parâmetro a conexão de desenvolvimento
 
+module.exports = connection;
