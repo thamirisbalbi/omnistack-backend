5ec237fab2181b8b3ecb8459eef970c9c74b1a2d
Thamiris Balbi <thamirisbalbi2277@gmail.com>
2022-02-16 03:59:11 -0300

    Importa pacote crypto para uso de funcionalidade

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 const express = require('express'); //importa a aplicação express para o arquivo routes
+const crypto = require('crypto');
 
 const routes = express.Router(); //desacopla as rotas do express em uma nova variável
 
 routes.post('/ongs', (request, response) => { //cria cadastro de ong com método post
     const { name, email, whatsapp, city, uf } = request.body; //acessar o corpo da requisição
+
+
 
     return response.json();
 }); 
