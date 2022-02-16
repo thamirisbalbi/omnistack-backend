fa39c4e4e781d3cea4a621de1c44e4bfb5de40a0
Thamiris Balbi <thamirisbalbi2277@gmail.com>
2022-02-16 03:08:31 -0300

    Cria os campos da tabela e o relacionamento

--- src/database/migrations/20220216054336_create_incidents.js
+++ src/database/migrations/20220216054336_create_incidents.js
@@ -1,7 +1,15 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('incidents', function(table){
+        table.increments(); //os casos cadastrados são auto incrementados na numeração
+        table.string('title').notNullable(); //nome da tabela, não pode ser nulo
+        table.string('description').notNullable();
+        table.decimal('value').notNullable(); //valor armazenado em decimais
+        
+       table.string('ong_id'),notNullTable() //coluna que armazena o id da ong (em string) que criou o incidente(relacionamento)
+    }); 
 };
+
 
 exports.down = function(knex) {
   
