6e7701b75bb59096e534050dc5396417a9a36fb7
Thamiris Balbi <thamirisbalbi2277@gmail.com>
2022-02-23 12:59:08 -0300

    Tira a criação de ongs do routes para esse arquivo

--- src/controllers/OngController.js
+++ src/controllers/OngController.js
@@ -2,6 +2,20 @@
 module.exports = {
     async create(request, response)
     {
-        
+        const { name, email, whatsapp, city, uf } = request.body; //acessar o corpo da requisição
+
+        const id = crypto.randomBytes(4).toString('HEX'); //gera 4 bytes de caracteres aleatórios, e os converte para string hexadecimal
+
+        await connection('ongs').insert({  //conexão com banco de dados
+            id,
+            name,
+            email,
+            whatsapp,
+            city,
+            uf,
+        })
+
+        return response.json( { id }); //devolve ao cliente na rota apenas o id da ong, pois com ele a ong se conecta à aplicação
+
     }    //método create 
 };
