509185960bee449da8e030ce1176aa2b6ec96fa6
Thamiris Balbi <thamirisbalbi2277@gmail.com>
2022-02-23 12:47:06 -0300

    Adiciona commit em routes.js

--- src/routes.js
+++ src/routes.js
@@ -29,6 +29,6 @@ routes.post('/ongs', async (request, response) => { //cria cadastro de ong com m
     }) //connection ('nome da tabela que se quer inserir dados').comando para inserir dados dentro da tabela
     //dentro do insert terá todas as informações da tabela de ongs
     return response.json( { id }); //devolve ao cliente na rota apenas o id da ong, pois com ele a ong se conecta à aplicação
-}); 
+}); //rota de criação da ong
 
 module.exports = routes; //deixa as rotas disponíveis para o acesso: exporta a variável routes de dentro do arquivo
