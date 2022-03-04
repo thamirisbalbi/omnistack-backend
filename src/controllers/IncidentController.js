218706eaa86e9469c5567da6d33794341fbf171a
Thamiris Balbi <thamirisbalbi2277@gmail.com>
2022-03-04 00:04:33 -0300

    Acessa cabeçalho da requisição

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -2,6 +2,7 @@ const connection = require('../database/connection');
 
 module.exports = {
     async create(request, response) {
-        const { title, description, value } = request.body; 
+        const { title, description, value } = request.body;
+        request.headers;
     }   //cria incidente
-} //exporta objeto
+}; //exporta objeto
