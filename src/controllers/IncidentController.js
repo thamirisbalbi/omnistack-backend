4ef4c37955ede8a69b1bc78c8689099b872776c8
Thamiris Balbi <thamirisbalbi2277@gmail.com>
2022-03-03 23:55:56 -0300

    Criação de estruturação do corpo da requisição

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -2,6 +2,6 @@ const connection = require('../database/connection');
 
 module.exports = {
     async create(request, response) {
-
+        const { title, description, value } = request.body; 
     }   //cria incidente
 } //exporta objeto
