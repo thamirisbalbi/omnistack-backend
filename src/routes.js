100ca9290fd5d3bded18474e7e0b7dd119ded7d7
Thamiris Balbi <thamirisbalbi2277@gmail.com>
2022-03-03 23:34:03 -0300

    Muda o comando de routes para OngController

--- src/routes.js
+++ src/routes.js
@@ -6,14 +6,6 @@ const connection = require('./database/connection');
 
 const routes = express.Router(); //desacopla as rotas do express em uma nova variável
 
-//routes.get('/ongs', async (request, response) => {
-
-//    const ongs = await connection('ongs').select('*'); // seleciona todos os campos de todos os registros dentro da tabela ongs 
-
-//    return response.json(ongs);
-     
-//}); //rota do tipo get, pois se quer listar algo
-
 routes.post('/ongs', OngController.create); //cria cadastro de ong com método post
  
 //rota de criação da ong
