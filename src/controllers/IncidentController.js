7d374ce92c2819095d76a1ee2070aa590cd0e7c3
Thamiris Balbi <thamirisbalbi2277@gmail.com>
2022-03-04 01:13:14 -0300

    Implementa aplicação de deletar caso

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const connection = require('../database/connection');
 
 module.exports = {
@@ -24,6 +25,18 @@ module.exports = {
     },
     
     async delete(request, respose) {
+        const { id } = request.params; //pegar o id no parâmetro de rotas
+        const ong_id = request.headers.authorization; // busca o id da ong, para verificar se é a ong que criou o caso quem está querendo deletá-lo
+
+        const incident = await connection('incidents')
+            .where('id', id) //onde o id for igual ao atribuído acima
+            .select('ong_id') //seleciona apenas a coluna ong_id
+            .first() //como retorna apenas um registro, é possível usar esse comando
+        
+        if (incident.ong_id != ong_id) {
+            return response.status(401).json({ error: 'Operation not permitted.' }); //status não autorizado, enviando uma resposta escrita em formato json
+        }
+
         
     }
 
