0ff373cdaec467f77d4fa091889eb8a73e3a3341
Thamiris Balbi <thamirisbalbi2277@gmail.com>
2022-03-04 00:13:10 -0300

    Insere os dados na tabela incidents

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -4,6 +4,12 @@ module.exports = {
     async create(request, response) {
         const { title, description, value } = request.body;
         const ong_id = request.headers.authorization;
-        
+
+        await connection('incidents').insert({
+            title,
+            description,
+            value,
+            ong_id,
+        });
     }   //cria incidente
 }; //exporta objeto
