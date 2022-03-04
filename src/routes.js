fe2fa8700f9b141115764677af3abab3788760c7
Thamiris Balbi <thamirisbalbi2277@gmail.com>
2022-03-04 00:25:15 -0300

    Importa dados de IncidentController

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 const express = require('express'); //importa a aplicação express para o arquivo routes
 
 const OngController = require('./controllers/OngController'); //importa dados de ongcontroller dentro da aplicação 
+const IncidentController = require('./controllers/IncidentController'); 
+
 
 //apaga comando de conexão com banco por ela não ser mais utilizada nas rotas 
 
@@ -9,6 +11,6 @@ const routes = express.Router(); //desacopla as rotas do express em uma nova var
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create); //cria cadastro de ong com método post
   
-routes.post('/incidents'); 
+routes.post('/incidents', IncidentController.create);
 
 module.exports = routes; //deixa as rotas disponíveis para o acesso: exporta a variável routes de dentro do arquivo
