4428f5d0cd372fcb86e004f6ae49b4e42a4e6c34
Thamiris Balbi <thamirisbalbi2277@gmail.com>
2022-03-04 00:25:49 -0300

    Cria rotas para os incidentes

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,6 @@ const routes = express.Router(); //desacopla as rotas do express em uma nova var
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create); //cria cadastro de ong com método post
   
-routes.post('/incidents', IncidentController.create);
+routes.post('/incidents', IncidentController.create); 
 
 module.exports = routes; //deixa as rotas disponíveis para o acesso: exporta a variável routes de dentro do arquivo
