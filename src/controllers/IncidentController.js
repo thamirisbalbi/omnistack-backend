ac05bddafe2eddb1526a944281ee047bdb718a18
Thamiris Balbi <thamirisbalbi2277@gmail.com>
2022-03-04 00:15:44 -0300

    Retorna id da ong, primeira opção de estruturação

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -5,11 +5,14 @@ module.exports = {
         const { title, description, value } = request.body;
         const ong_id = request.headers.authorization;
 
-        await connection('incidents').insert({
+        const result = await connection('incidents').insert({
             title,
             description,
             value,
             ong_id,
         });
+
+        const id = result[0]; //resultado será um array de uma posição, retornando o id
+
     }   //cria incidente
 }; //exporta objeto
