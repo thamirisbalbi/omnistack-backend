8db6b4c38e41538b098bb8cd4393b84838e98feb
Thamiris Balbi <thamirisbalbi2277@gmail.com>
2022-03-04 00:10:14 -0300

    Requisita do cabeçalho o id da ong

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -3,6 +3,7 @@ const connection = require('../database/connection');
 module.exports = {
     async create(request, response) {
         const { title, description, value } = request.body;
-        request.headers;
+        const ong_id = request.headers.authorization;
+        
     }   //cria incidente
 }; //exporta objeto
